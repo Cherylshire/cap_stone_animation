3679bc72f2f920d071175d03bf3839734fb87b62
“Cherylshire” <hsuanchang2017@gmail.com>
2019-11-08 16:59:37 -0600

    making negative space Global_Composite_Operation

--- boolean/boolean.js
+++ boolean/boolean.js
@@ -5,39 +5,216 @@ const ctx = canvas.getContext('2d');
 ctx.globalCompositeOperation = 'xor';
 
 ctx.fillStyle = 'navy';
+ctx.fillRect(0, 0, 30, 30);
+
+ctx.fillStyle = 'white';
+ctx.fillRect(10, 0, 10, 10);
+
+ctx.fillStyle = 'navy';
+ctx.fillRect(10, 10, 10, 10);
+
+ctx.fillStyle = 'white';
+ctx.fillRect(0, 10, 10, 10);
+
+ctx.fillStyle = 'navy';
+ctx.fillRect(10, 10, 30, 30);
+
+ctx.fillStyle = 'navy';
+ctx.fillRect(20, 20, 30, 30);
+
+ctx.fillStyle = 'navy';
+ctx.fillRect(30, 30, 30, 30);
+
+ctx.fillStyle = 'navy';
+ctx.fillRect(40, 40, 30, 30);
+
+ctx.fillStyle = 'navy';
+ctx.fillRect(40, 0, 30, 30);
+
+ctx.fillStyle = 'navy';
+ctx.fillRect(0, 40, 30, 30);
+
+ctx.fillStyle = 'navy';
+ctx.fillRect(50, 50, 30, 30);
+
+ctx.fillStyle = 'navy';
+ctx.fillRect(60, 60, 30, 30);
+
+ctx.fillStyle = 'navy';
+ctx.fillRect(60, 0, 30, 30);
+
+ctx.fillStyle = 'navy';
+ctx.fillRect(0, 60, 30, 30);
+
+ctx.fillStyle = 'navy';
+ctx.fillRect(70, 70, 30, 30);
+
+ctx.fillStyle = 'navy';
+ctx.fillRect(80, 80, 30, 30);
+
+ctx.fillStyle = 'navy';
+ctx.fillRect(90, 90, 30, 30);
+
+ctx.fillStyle = 'navy';
+ctx.fillRect(100, 100, 30, 30);
+
+ctx.fillStyle = 'navy';
+ctx.fillRect(110, 110, 30, 30);
+
+ctx.fillStyle = 'navy';
+ctx.fillRect(120, 120, 30, 30);
+
+ctx.fillStyle = 'navy';
+ctx.fillRect(130, 130, 30, 30);
+
+ctx.fillStyle = 'navy';
+ctx.fillRect(140, 140, 30, 30);
+
+// ==================================
+
+ctx.fillStyle = '#abcdef';
+ctx.fillRect(0, 0, 100, 100);
+
+ctx.fillStyle = '#fedcba';
+ctx.fillRect(0, 0, 95, 95);
+
+ctx.fillStyle = '#abcdef';
+ctx.fillRect(0, 0, 90, 90);
+
+ctx.fillStyle = '#fedcba';
+ctx.fillRect(0, 0, 85, 85);
+
+ctx.fillStyle = '#abcdef';
+ctx.fillRect(0, 0, 80, 80);
+
+ctx.fillStyle = '#fedcba';
+ctx.fillRect(0, 0, 75, 75);
+
+ctx.fillStyle = '#abcdef';
+ctx.fillRect(0, 0, 70, 70);
+
+ctx.fillStyle = '#fedcba';
+ctx.fillRect(0, 0, 65, 65);
+
+ctx.fillStyle = '#abcdef';
+ctx.fillRect(0, 0, 60, 60);
+
+ctx.fillStyle = '#fedcba';
+ctx.fillRect(0, 0, 55, 55);
+
+ctx.fillStyle = '#abcdef';
 ctx.fillRect(0, 0, 50, 50);
 
-ctx.fillStyle = 'purple';
-ctx.fillRect(10, 10, 50, 50);
+ctx.fillStyle = '#fedcba';
+ctx.fillRect(0, 0, 45, 45);
 
-ctx.fillStyle = 'teal';
-ctx.fillRect(20, 20, 50, 50);
+ctx.fillStyle = '#abcdef';
+ctx.fillRect(0, 0, 40, 40);
 
-ctx.fillStyle = 'blue';
-ctx.fillRect(30, 30, 50, 50);
+ctx.fillStyle = '#fedcba';
+ctx.fillRect(0, 0, 35, 35);
 
-ctx.fillStyle = 'green';
-ctx.fillRect(40, 40, 50, 50);
+ctx.fillStyle = '#abcdef';
+ctx.fillRect(0, 0, 30, 30);
 
-ctx.fillStyle = 'pink';
-ctx.fillRect(50, 50, 50, 50);
+ctx.fillStyle = '#fedcba';
+ctx.fillRect(0, 0, 25, 25);
 
-ctx.fillStyle = 'red';
-ctx.fillRect(60, 60, 50, 50);
+ctx.fillStyle = '#abcdef';
+ctx.fillRect(0, 0, 20, 20);
 
-ctx.fillStyle = 'yellow';
-ctx.fillRect(70, 70, 50, 50);
+ctx.fillStyle = '#fedcba';
+ctx.fillRect(0, 0, 15, 15);
 
-ctx.fillStyle = 'orange';
-ctx.fillRect(80, 80, 50, 50);
+ctx.fillStyle = '#abcdef';
+ctx.fillRect(0, 0, 10, 10);
 
-ctx.fillStyle = 'brown';
-ctx.fillRect(90, 90, 50, 50);
-// xor
-// ctx.globalCompositeOperation = 'xor';
+ctx.fillStyle = '#fedcba';
+ctx.fillRect(0, 0, 5, 5);
 
-// ctx.fillStyle = 'blue';
-// ctx.fillRect(50, 50, 100, 100);
+ctx.fillStyle = '#fedcba';
+ctx.fillRect(5, 5, 100, 100);
 
-// ctx.fillStyle = 'red';
-// ctx.fillRect(50, 50, 100, 100);
+ctx.fillStyle = '#abcdef';
+ctx.fillRect(10, 10, 100, 100);
+
+ctx.fillStyle = '#fedcba';
+ctx.fillRect(15, 15, 100, 100);
+
+ctx.fillStyle = '#abcdef';
+ctx.fillRect(20, 20, 100, 100);
+
+ctx.fillStyle = '#fedcba';
+ctx.fillRect(25, 25, 100, 100);
+
+ctx.fillStyle = '#abcdef';
+ctx.fillRect(30, 30, 100, 100);
+
+ctx.fillStyle = '#fedcba';
+ctx.fillRect(35, 35, 100, 100);
+
+ctx.fillStyle = '#abcdef';
+ctx.fillRect(40, 40, 100, 100);
+
+ctx.fillStyle = '#fedcba';
+ctx.fillRect(45, 45, 100, 100);
+
+ctx.fillStyle = '#abcdef';
+ctx.fillRect(50, 50, 100, 100);
+
+ctx.fillStyle = '#fedcba';
+ctx.fillRect(55, 55, 100, 100);
+
+ctx.fillStyle = '#abcdef';
+ctx.fillRect(60, 60, 100, 100);
+
+ctx.fillStyle = '#fedcba';
+ctx.fillRect(65, 65, 100, 100);
+
+ctx.fillStyle = '#abcdef';
+ctx.fillRect(70, 70, 100, 100);
+
+ctx.fillStyle = '#fedcba';
+ctx.fillRect(75, 75, 100, 100);
+
+ctx.fillStyle = '#abcdef';
+ctx.fillRect(80, 80, 100, 100);
+
+ctx.fillStyle = '#fedcba';
+ctx.fillRect(85, 85, 100, 100);
+
+ctx.fillStyle = '#abcdef';
+ctx.fillRect(90, 90, 100, 100);
+
+ctx.fillStyle = '#fedcba';
+ctx.fillRect(95, 95, 100, 100);
+
+ctx.fillStyle = '#abcdef';
+ctx.fillRect(100, 100, 100, 100);
+
+ctx.fillStyle = '#fedcba';
+ctx.fillRect(105, 105, 100, 100);
+
+ctx.fillStyle = '#abcdef';
+ctx.fillRect(110, 110, 100, 100);
+
+ctx.fillStyle = '#fedcba';
+ctx.fillRect(115, 115, 100, 100);
+
+ctx.fillStyle = '#abcdef';
+ctx.fillRect(120, 120, 100, 100);
+
+ctx.fillStyle = '#fedcba';
+ctx.fillRect(125, 125, 100, 100);
+
+ctx.fillStyle = '#abcdef';
+ctx.fillRect(130, 130, 100, 100);
+
+ctx.fillStyle = '#fedcba';
+ctx.fillRect(135, 135, 100, 100);
+
+ctx.fillStyle = '#abcdef';
+ctx.fillRect(140, 140, 100, 100);
+
+ctx.fillStyle = '#fedcba';
+ctx.fillRect(145, 145, 100, 100);
